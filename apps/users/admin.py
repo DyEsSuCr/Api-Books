9100860d8c01284dff94e730e4273d06e0514049
DylanSuarez <2000sajo@gmail.com>
2023-01-30 23:08:41 -0500

    model user

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from apps.users.models import User
 
 # Register your models here.
+
+class CustomUser(admin.ModelAdmin):
+  list_display = ('username', 'avatar', 'created_at')
+  list_editable = ('avatar', )
+
+admin.site.register(User, CustomUser)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+  avatar = models.ImageField('Avatar', default='avatars/default-avatar.png', upload_to='avatars', blank=True, null=True)
+  created_at = models.DateTimeField('Fecha de creación', auto_now=True, auto_now_add=False)
+
+  class Meta:
+    ordering = ["created_at"]
+    db_table = 'auth_user'
+    verbose_name = 'Usuario'
+    verbose_name_plural = 'Usuarios'
+
+  @property
+  def get_photo(self):
+    if self.avatar:
+      return self.avatar.url
