fbcfdd651aef6377fab6c253c49293fdbcc3d15d
DylanSuarez <2000sajo@gmail.com>
2023-02-01 21:57:57 -0500

    models (book-gender)

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.books.models import Book, Gender
 
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Gender)
--- apps/books/models.py
+++ apps/books/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Gender(models.Model):
+  name = models.CharField('Genero', max_length=120, unique=True)
+
+  class Meta:
+    ordering = ['name']
+    db_table = 'gender'
+    verbose_name = 'Genero'
+    verbose_name_plural = 'Generos'
+
+  def __str__(self):
+    return f'{self.name}'
+
+
+class Book(models.Model):
+  title = models.CharField('Titulo', max_length=120, unique=True)
+  author = models.ManyToManyField("authors.Author", verbose_name="Author")
+  gender = models.ManyToManyField("Gender", verbose_name="Genero")
+
+  class Meta:
+    ordering = ['title']
+    db_table = 'book'
+    verbose_name = 'Libro'
+    verbose_name_plural = 'Libros'
+
+  def __str__(self):
+    return f'{self.title}'
+  
